f9ad72a3aa045c6fdeb13b2d3fa2a930c7504611
qqbeach <danildzekelev@gmail.com>
2023-12-10 20:25:00 +0200

    Add booleans hasBlackJack and isAlive.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 let firstCard = Math.floor(Math.random() * 10) + 2; // write unit test
 let secondCard = Math.floor(Math.random() * 10) + 2;
-
+let hasBlackJack = false
+let isAlive = true
 
 let sum = firstCard + secondCard;
+console.log(sum)
 
 if (sum < 21) {
     console.log("Wanna draw a new card?")
 } else if (sum === 21) {
     console.log("You won!")
+    hasBlackJack = true
 } else {
     console.log("You lost!")
+    isAlive = false
 }

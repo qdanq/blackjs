f51606cd6125d89298884870d4e65d33642f2309
qqbeach <danildzekelev@gmail.com>
2023-12-10 20:18:11 +0200

    Write ifelse statements.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ let secondCard = Math.floor(Math.random() * 10) + 2;
 
 
 let sum = firstCard + secondCard;
+
+if (sum < 21) {
+    console.log("Wanna draw a new card?")
+} else if (sum === 21) {
+    console.log("You won!")
+} else {
+    console.log("You lost!")
+}

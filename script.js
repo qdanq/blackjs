a918c74e962e08d82963c9ad4a19df33069967a8
qqbeach <danildzekelev@gmail.com>
2023-12-14 20:39:03 +0200

    Fix bug with script loading in index.html file.

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-let firstCard = Math.floor(Math.random() * 10) + 2; // write unit test
+let firstCard = Math.floor(Math.random() * 10) + 2;
 let secondCard = Math.floor(Math.random() * 10) + 2;
-let hasBlackJack = false
-let isAlive = true
-let message = ""
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message");
 
 let sum = firstCard + secondCard;
-console.log(sum)
+console.log(sum);
 
 function startGame() {
   if (sum < 21) {
-    message = "Do you want to draw a card"
-} else if (sum === 21) {
-    message = "Blackjack! You won!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
-}
-console.log(message)
+    message = "Do you want to draw a card?";
+  } else if (sum === 21) {
+    message = "Blackjack! You won!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
 
+  messageEl.textContent = message;
+  
 }

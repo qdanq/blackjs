865764e7b561824d8297a09653889d66793553a5
qqbeach <danildzekelev@gmail.com>
2023-12-14 21:07:02 +0200

    Delete useless variable.

--- script.js
+++ script.js
@@ -2,23 +2,21 @@ let firstCard = Math.floor(Math.random() * 10) + 2;
 let secondCard = Math.floor(Math.random() * 10) + 2;
 let hasBlackJack = false;
 let isAlive = true;
-let message = "";
-let messageEl = document.getElementById("message");
+let message = document.getElementById("message");
 
-let sum = firstCard + secondCard + 10;
+let sum = firstCard + secondCard;
 console.log(sum);
 
 function startGame() {
   if (sum < 21) {
-    message = "Do you want to draw a card?";
+    message.textContent = "Do you want to draw a card?";
   } else if (sum === 21) {
-    message = "Blackjack! You won!";
+    message.textContent = "Blackjack! You won!";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game!";
+    message.textContent = "You're out of the game!";
     isAlive = false;
   }
 
-  messageEl.textContent = message;
   
 }
